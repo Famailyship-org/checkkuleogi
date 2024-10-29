bcbe7f9468de71a0e99c266a22dd5bda2f4d1cc4
daase <abstar@gachon.ac.kr>
2024-10-29 16:47:22 +0900

    Refactoring(config): cors config 수정

--- src/main/java/com/Familyship/checkkuleogi/config/SecurityConfig.java
+++ src/main/java/com/Familyship/checkkuleogi/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.Familyship.checkkuleogi.config;
 
-import com.Familyship.checkkuleogi.domains.user.service.CustomUserDetailsService;
 import com.Familyship.checkkuleogi.security.jwt.JwtFilter;
 import com.Familyship.checkkuleogi.security.jwt.JwtProvider;
 
@@ -9,7 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.security.config.Customizer;
+
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
