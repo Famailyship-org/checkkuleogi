cf6394007d6f45499ee25615e048f98793542016
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-10-30 02:57:24 +0900

    feat(batch): 휴면유저 isActive 배치 작업

--- src/main/java/com/Familyship/checkkuleogi/CheckkuleogiApplication.java
+++ src/main/java/com/Familyship/checkkuleogi/CheckkuleogiApplication.java
@@ -2,8 +2,10 @@ package com.Familyship.checkkuleogi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CheckkuleogiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/Familyship/checkkuleogi/domains/child/domain/repository/ChildRepository.java
+++ src/main/java/com/Familyship/checkkuleogi/domains/child/domain/repository/ChildRepository.java
@@ -2,9 +2,23 @@ package com.Familyship.checkkuleogi.domains.child.domain.repository;
 
 import com.Familyship.checkkuleogi.domains.child.domain.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface ChildRepository extends JpaRepository<Child, Long> {
     Optional<Child> findByName(String name);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Child c SET c.isActive = true WHERE c.idx IN :activeUserIds")
+    void markUsersAsActive(Set<Long> activeUserIds);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Child c SET c.isActive = false WHERE c.idx NOT IN :activeUserIds")
+    void markUsersAsInactiveNotIn(Set<Long> activeUserIds);
 }
