3c237c5d53b740f4062d43e8426c019e829131d1
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-11-01 17:53:15 +0900

    feat(recommend): 추천 목록 get API 최적화

--- src/main/java/com/Familyship/checkkuleogi/domains/recommend/domain/Recommend.java
+++ src/main/java/com/Familyship/checkkuleogi/domains/recommend/domain/Recommend.java
@@ -11,6 +11,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+//@Table(
+//        indexes = {
+//                @Index(name = "recommend", columnList = "childIdx")
+//        })  //추후 Recommend에 속상이 늘어날 시 인덱싱 트레이드오프 계산 필요
 public class Recommend {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
